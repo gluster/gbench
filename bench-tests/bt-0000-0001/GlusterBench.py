9c4b47ae26363b47b82e004f0450fc6c90d2ff45
Ben Turner <bennyturns@gmail.com>
2016-10-06 15:44:06 -0400

    Enhanced help output and fix error with smallfile check
    Signed-off-by: Ben Turner <bennyturns@gmail.com>

--- bench-tests/bt-0000-0001/GlusterBench.py
+++ bench-tests/bt-0000-0001/GlusterBench.py
@@ -138,6 +138,12 @@ def main():
         sys.exit(0)
 
 def usage():
+    print "Gluster Benchmark Kit Options:"
+    print "  -h --help           Print gbench options."
+    print "  -v                  Verbose Output."
+    print "  -s --size           Record size for large files, file size for small files."
+    print "  -f --files          The nuber of files to create for smallfile tests."
+    print "  -n --sample-size    The number of samples to collect for each test."
     print "Example: GlusterBench.py -s 64 -f 10000 -n 5 -v\n"
 
 def make_report(size, average_seq_write, average_seq_read, average_rand_write, average_rand_read, average_smallfile_create, average_smallfile_read, average_smallfile_ls):
